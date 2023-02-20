7c39846133bd3258070d0aaf17f3480863532d7f
Lorax-exe <lorissakir04@gmail.com>
2023-02-20 13:24:44 +0100

    terzo commit

--- ClientMain.java
+++ ClientMain.java
@@ -6,30 +6,13 @@ import java.net.UnknownHostException;
 import java.io.IOException;
 
 public class ClientMain {
-    public static void main(String[] args){
-        try {
-            DatagramSocket socket = new DatagramSocket();
-            byte[] data = "Hello, world!".getBytes();
-            InetAddress address;
-            try {
-                address = InetAddress.getByName("localhost");
-                int port = 12345;
-                DatagramPacket packet = new DatagramPacket(data, data.length, address, port); 
-                try {
-                    socket.send(packet);
-                    socket.close();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            } catch (UnknownHostException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-
-        } catch (SocketException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws SocketException, UnknownHostException, IOException {
+        DatagramSocket socket = new DatagramSocket();
+        byte[] data = "Hello, world!".getBytes();
+        InetAddress address = InetAddress.getByName("localhost");
+        int port = 12345;
+        DatagramPacket packet = new DatagramPacket(data, data.length, address, port); 
+        socket.send(packet);
+        socket.close();
     }
 }
